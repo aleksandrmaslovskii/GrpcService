6007df11d31670765a3512c6bf7f1261f9b2522f
Aleksandr Maslovskii <aleksandr.maslovskii.1990@gmail.com>
2022-06-16 00:34:49 +0300

    GrpcClient

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Net.Client;
+using GrpcService;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7288");
+
+var client = new Greeter.GreeterClient(channel);
+Console.Write("Enter name: ");
+var name = Console.ReadLine();
+
+var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+Console.WriteLine("Server response: " + reply.Message);
+Console.ReadKey();
